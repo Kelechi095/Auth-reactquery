a7265a09e20cda5d771dd6efc30c66b9ecd4cd5b
Awoms Kelechi <awomskelechi@gmail.com>
2023-10-19 20:37:36 +0100

    modified code

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api": {
+      "/": {
         target: env.VITE_API_ENDPOINT,
         secure: false
       },

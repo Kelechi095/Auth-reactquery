fab41a03fdaa5c348fe31cd5485224307a53c01a
Awoms Kelechi <awomskelechi@gmail.com>
2023-10-19 21:37:03 +0100

    code refactor

--- src/components/firebase/firebase.js
+++ src/components/firebase/firebase.js
@@ -1,14 +1,12 @@
-
-
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  apiKey: "AIzaSyDFFYeMqKeCZxV70iaHlpnNzJEQgxH9PfU",
   authDomain: "mern-auth-881dd.firebaseapp.com",
   projectId: "mern-auth-881dd",
   storageBucket: "mern-auth-881dd.appspot.com",
   messagingSenderId: "615183990641",
-  appId: "1:615183990641:web:0df13e93b439d321dc40d0"
+  appId: "1:615183990641:web:0df13e93b439d321dc40d0",
 };
 
 // Initialize Firebase

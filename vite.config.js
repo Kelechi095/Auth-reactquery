7c483adfb501e3cbc49f84d3c98ef59692f6322f
Awoms Kelechi <awomskelechi@gmail.com>
2023-10-19 20:44:46 +0100

    code refactoring

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,6 @@
-import { defineConfig, loadEnv } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig} from 'vite'
+import react from '@vitejs/plugin-react'
 
-const env = loadEnv("all", process.cwd());
-
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/": {
-        target: env.VITE_API_ENDPOINT,
-        secure: false
-      },
-    },
-  },
-});
+})

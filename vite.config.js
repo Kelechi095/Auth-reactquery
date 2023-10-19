00ff33823a24537973866952e1a4772b2a9d12c8
Awoms Kelechi <awomskelechi@gmail.com>
2023-10-19 20:56:26 +0100

    code refactor

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,20 @@
 import { defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
+/* const env = loadEnv(
+  'all',
+  process.cwd()
+);
+ */
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: "https://kelmauth.onrender.com",
+        changeOrigin: true
+      }
+    }
+  }
 })

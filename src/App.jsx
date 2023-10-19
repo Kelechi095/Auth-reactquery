20cfb19a58127a8dc7ccc6b06e2ca4239c0e3a06
Awoms Kelechi <awomskelechi@gmail.com>
2023-10-19 22:55:16 +0100

    refactor

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,20 @@ import Profile from "./pages/Profile";
 import About from "./pages/About";
 import Login from "./pages/Login";
 import Layout from "./components/Layout";
+import ProtectedRoute from "./pages/ProtectedRoute";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout />}>
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Layout />
+            </ProtectedRoute>
+          }
+        >
           <Route index element={<Home />} />
           <Route path="/profile" element={<Profile />} />
           <Route path="/about" element={<About />} />

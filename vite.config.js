b49b1ffc2853f1588ef2d2a92a8f8a01c417f859
Awoms Kelechi <awomskelechi@gmail.com>
2023-10-19 18:40:02 +0100

    code refactor

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
-const env = loadEnv(
+/* const env = loadEnv(
   'all',
   process.cwd()
 );
+ */
 
 // https://vitejs.dev/config/
 export default defineConfig({
